643350e4d5f26dd51fd543b57158b0d61606f55a
raheelimran-10P <raheel.imran@10pearls.com>
2022-10-04 14:48:02 +0500

    Resolve warning on setup driver

--- Browser/Tests/conftest.py
+++ Browser/Tests/conftest.py
@@ -1,13 +1,17 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 
 
 @pytest.fixture(params=["chrome"], scope='class')
 def init_driver(request):
+    options = Options()
+    options.add_argument("start-maximized")
     #if request.param == "Chrome":
-    web_driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
+    web_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     #if request.param == "Firefox":
     #    web_driver = webdriver.firefox(executable_path=GeckoDriverManager().install())
     request.cls.driver = web_driver
