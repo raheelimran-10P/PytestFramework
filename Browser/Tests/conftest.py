9aea5eaac1e32a84ecd604c6dbdc4ce4cdb7896a
raheelimran-10P <raheel.imran@10pearls.com>
2022-10-04 15:44:59 +0500

    Add conda env file

--- Browser/Tests/conftest.py
+++ Browser/Tests/conftest.py
@@ -11,7 +11,8 @@ def init_driver(request):
     options = Options()
     options.add_argument("start-maximized")
     #if request.param == "Chrome":
-    web_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    #web_driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    web_driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
     #if request.param == "Firefox":
     #    web_driver = webdriver.firefox(executable_path=GeckoDriverManager().install())
     request.cls.driver = web_driver
