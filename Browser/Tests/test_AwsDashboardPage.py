81575d07b67405a8949df867388c0afea1448ff8
raheelimran-10P <raheel.imran@10pearls.com>
2023-01-27 15:49:11 +0500

    Update

--- Browser/Tests/test_AwsDashboardPage.py
+++ Browser/Tests/test_AwsDashboardPage.py
@@ -1,3 +1,4 @@
+import os
 import time
 import pytest
 from decouple import config
@@ -9,6 +10,7 @@ from Browser.Pages.AwsLoginPage import AwsLoginPage
 from Browser.Pages.DashboardPage import DashboardPage
 from Browser.TestData.TestData import TestData
 from Browser.Tests.test_base import BaseTest
+from Utilities.script_01 import script_01
 
 
 class TestAwsDashboardPage(BaseTest):
@@ -74,6 +76,27 @@ class TestAwsDashboardPage(BaseTest):
         aws_DashboardPage = self.driver.current_window_handle
         self.AwsDashboardPage.switchTab(aws_ConsoleDashboardPage)
         self.AwsConsoleDashboardPage.publish_to_a_topic(TestData.TOPIC_NAME, False, False)
+        self.AwsConsoleDashboardPage.wait(10)
+
+        object_01 = script_01()
+        path = "/media/raheel/22687D06687CD9CD/projects/groundowl-app/appData/"
+        text_01 = "mock_geophex_v2_raw_emi"
+        text_02 = "mock_geophex_v2_raw_gps"
+        text_03 = "mock_ml_model_v1_till_depth"
+        text_04 = "IDSGPRMock"
+
+        print(text_01, object_01.is_data_exported(path, text_01))
+        print(text_02, object_01.is_data_exported(path, text_02))
+        print(text_03, object_01.is_data_exported(path, text_03))
+        print(text_04, object_01.is_data_exported(path, text_04))
+
+        if (object_01.is_data_exported(path, text_01)) and (object_01.is_data_exported(path, text_02)) and (
+                object_01.is_data_exported(path, text_03)) and (object_01.is_data_exported(path, text_04)):
+            print("all passed")
+
+        else:
+            raise Exception
+            print("not all passed")
 
 
 
