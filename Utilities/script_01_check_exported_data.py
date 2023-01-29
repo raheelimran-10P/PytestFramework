ecfe96d4fab8e16184888dbda2cff4b591c64d73
raheelimran-10P <raheel.imran@10pearls.com>
2023-01-29 09:47:47 +0500

    Remove used code

--- Utilities/script_01_check_exported_data.py
+++ Utilities/script_01_check_exported_data.py
@@ -47,31 +47,3 @@ class script_01_check_exported_data:
                 os.remove(file)
         for dir in dirs:
             os.rmdir(dir)
-
-object_01 = script_01_check_exported_data()
-
-path = "/media/raheel/22687D06687CD9CD/projects/groundowl-app/appData/"
-path2 = "/media/raheel/22687D06687CD9CD/projects/groundowl-app/appState/groundowl-canbus/app_state.json"
-texts = ["mock_geophex_v2_raw_emi", "mock_geophex_v2_raw_gps", "mock_ml_model_v1_till_depth", "IDSGPRMock"]
-text_01 = "mock_geophex_v2_raw_emi"
-text_02 = "mock_geophex_v2_raw_gps"
-text_03 = "mock_ml_model_v1_till_depth"
-text_04 = "IDSGPRMock"
-
-print(text_01, object_01.is_data_exported(path, text_01))
-print(text_02, object_01.is_data_exported(path, text_02))
-print(text_03, object_01.is_data_exported(path, text_03))
-print(text_04, object_01.is_data_exported(path, text_04))
-
-if (object_01.is_data_exported(path, text_01)) and (object_01.is_data_exported(path, text_02)) and (
-        object_01.is_data_exported(path, text_03)) and (object_01.is_data_exported(path, text_04)):
-    print("Exported data is verified!")
-    object_01.delete_all__exported_date(path)
-    print("Deleted all export data!")
-
-else:
-    print("Exported data is not verified!")
-    object_01.delete_all__exported_date(path)
-    print("Deleted all export data!")
-
-#os.remove("/media/raheel/22687D06687CD9CD/projects/groundowl-app/appState/groundowl-canbus/app_state.json")
