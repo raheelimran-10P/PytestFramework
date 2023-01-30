d126db97d565fe07365bc95c88e9def032b5906c
raheelimran-10P <raheel.imran@10pearls.com>
2023-01-30 10:14:55 +0500

    Parameterize test case and skip all unuse test case

--- Browser/Tests/test_AwsDashboardPage.py
+++ Browser/Tests/test_AwsDashboardPage.py
@@ -14,6 +14,7 @@ from Utilities.script_01_check_exported_data import script_01_check_exported_dat
 
 class TestAwsDashboardPage(BaseTest):
 
+    @pytest.mark.skip(reason="project setup locally (not hosted on any server)")
     def test_6(self):
         self.AwsLoginPage = AwsLoginPage(self.driver)
         self.AwsLoginPage.goToUrl(config('AWS_IOT_URL'))
@@ -26,6 +27,7 @@ class TestAwsDashboardPage(BaseTest):
         self.AwsDashboardPage.create_simulation()
         # self.AwsDashboardPage.run_simulations()
 
+    @pytest.mark.skip(reason="project setup locally (not hosted on any server)")
     def test_7(self):
         self.AwsLoginPage = AwsLoginPage(self.driver)
         self.AwsLoginPage.goToUrl(config('AWS_IOT_URL'))
@@ -38,16 +40,8 @@ class TestAwsDashboardPage(BaseTest):
         self.AwsDashboardPage.create_simulation()
         self.AwsDashboardPage.run_simulations()
 
+    @pytest.mark.skip(reason="project setup locally (not hosted on any server)")
     def test_8(self):
-        self.AwsConsoleLoginPage = AwsConsoleLoginPage(self.driver)
-        self.AwsConsoleLoginPage.goToUrl(config('AWS_Console_URL'))
-        self.AwsConsoleLoginPage.login(config('AWS_Console_ACCOUNT_ID'), config('AWS_Console_USERNAME'), config('AWS_Console_PASSWORD'))
-        self.AwsConsoleDashboardPage = AwsConsoleDashboardPage(self.driver)
-        self.AwsConsoleDashboardPage.is_visible(self.AwsConsoleDashboardPage.SUBSCRIBE_BUTTON)
-        self.AwsConsoleDashboardPage.subscribe_to_a_topic(TestData.TOPIC_FILTER)
-        self.AwsConsoleDashboardPage.publish_to_a_topic(TestData.TOPIC_NAME, True, True)
-
-    def test_9(self):
         self.AwsConsoleLoginPage = AwsConsoleLoginPage(self.driver)
         self.AwsConsoleLoginPage.goToUrl(config('AWS_Console_URL'))
         self.AwsConsoleLoginPage.login(config('AWS_Console_ACCOUNT_ID'), config('AWS_Console_USERNAME'),
@@ -56,31 +50,46 @@ class TestAwsDashboardPage(BaseTest):
         self.AwsConsoleDashboardPage.is_visible(self.AwsConsoleDashboardPage.SUBSCRIBE_BUTTON)
         self.AwsConsoleDashboardPage.subscribe_to_a_topic(TestData.TOPIC_FILTER)
         self.AwsConsoleDashboardPage.publish_to_a_topic(TestData.TOPIC_NAME, True, True)
+
+    @pytest.mark.parametrize("before_data_storage_req, before_sensor_inuse_flag, after_data_storage_req, after_sensor_inuse_flag", [
+        (True, True, False, False)
+    ])
+    def test_9(self, before_data_storage_req, before_sensor_inuse_flag, after_data_storage_req, after_sensor_inuse_flag):
+        self.AwsConsoleLoginPage = AwsConsoleLoginPage(self.driver)
+        self.AwsConsoleLoginPage.goToUrl(config('AWS_Console_URL'))
+        self.AwsConsoleLoginPage.login(config('AWS_Console_ACCOUNT_ID'), config('AWS_Console_USERNAME'),
+                                       config('AWS_Console_PASSWORD'))
+        self.AwsConsoleDashboardPage = AwsConsoleDashboardPage(self.driver)
+        self.AwsConsoleDashboardPage.is_visible(self.AwsConsoleDashboardPage.SUBSCRIBE_BUTTON)
+        self.AwsConsoleDashboardPage.subscribe_to_a_topic(TestData.TOPIC_FILTER)
+        self.AwsConsoleDashboardPage.publish_to_a_topic(TestData.TOPIC_NAME, before_data_storage_req, before_sensor_inuse_flag)
         aws_ConsoleDashboardPage = self.driver.current_window_handle
         self.AwsConsoleDashboardPage.openNewTab()
-
         self.AwsLoginPage = AwsLoginPage(self.driver)
         self.AwsLoginPage.goToUrl(config('AWS_IOT_URL'))
         self.AwsLoginPage.login(config('AWS_IOT_USERNAME'), config('AWS_IOT_PASSWORD'))
-
         self.AwsDashboardPage = AwsDashboardPage(self.driver)
         self.AwsDashboardPage.get_title('IoT Device Simulator')
-        # self.AwsDashboardPage.go_to_device_type()
-        # self.AwsDashboardPage.create_device_types()
+        self.AwsDashboardPage.go_to_device_type()
+        self.AwsDashboardPage.create_device_types()
         self.AwsDashboardPage.go_to_simulation()
-        # self.AwsDashboardPage.create_simulation()
+        self.AwsDashboardPage.create_simulation()
         self.AwsDashboardPage.run_simulations()
         aws_DashboardPage = self.driver.current_window_handle
         self.AwsDashboardPage.switchTab(aws_ConsoleDashboardPage)
-        self.AwsConsoleDashboardPage.publish_to_a_topic(TestData.TOPIC_NAME, False, False)
+        self.AwsConsoleDashboardPage.publish_to_a_topic(TestData.TOPIC_NAME, after_data_storage_req, after_sensor_inuse_flag)
         self.AwsConsoleDashboardPage.wait(10)
 
         object_01 = script_01_check_exported_data()
 
-        print(TestData.EMI_KEYWORD_IN_EXPORT_DATA, object_01.is_data_exported(TestData.APPDATA_PATH, TestData.EMI_KEYWORD_IN_EXPORT_DATA))
-        print(TestData.GPS_KEYWORD_IN_EXPORT_DATA, object_01.is_data_exported(TestData.APPDATA_PATH, TestData.GPS_KEYWORD_IN_EXPORT_DATA))
-        print(TestData.MLMODEL_KEYWORD_IN_EXPORT_DATA, object_01.is_data_exported(TestData.APPDATA_PATH, TestData.MLMODEL_KEYWORD_IN_EXPORT_DATA))
-        print(TestData.IDSGPR_KEYWORD_IN_EXPORT_DATA, object_01.is_data_exported(TestData.APPDATA_PATH, TestData.IDSGPR_KEYWORD_IN_EXPORT_DATA))
+        print(TestData.EMI_KEYWORD_IN_EXPORT_DATA,
+              object_01.is_data_exported(TestData.APPDATA_PATH, TestData.EMI_KEYWORD_IN_EXPORT_DATA))
+        print(TestData.GPS_KEYWORD_IN_EXPORT_DATA,
+              object_01.is_data_exported(TestData.APPDATA_PATH, TestData.GPS_KEYWORD_IN_EXPORT_DATA))
+        print(TestData.MLMODEL_KEYWORD_IN_EXPORT_DATA,
+              object_01.is_data_exported(TestData.APPDATA_PATH, TestData.MLMODEL_KEYWORD_IN_EXPORT_DATA))
+        print(TestData.IDSGPR_KEYWORD_IN_EXPORT_DATA,
+              object_01.is_data_exported(TestData.APPDATA_PATH, TestData.IDSGPR_KEYWORD_IN_EXPORT_DATA))
 
         if (object_01.is_data_exported(TestData.APPDATA_PATH, TestData.EMI_KEYWORD_IN_EXPORT_DATA)) and \
                 (object_01.is_data_exported(TestData.APPDATA_PATH, TestData.GPS_KEYWORD_IN_EXPORT_DATA)) and \
@@ -96,7 +105,3 @@ class TestAwsDashboardPage(BaseTest):
             print("Deleted all export data!")
 
         os.remove(TestData.APPSTATE_FILE_PATH)
-
-
-
-
