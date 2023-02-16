91b06dad170a605e1e99c6a20e4489045dc628a5
Muhammad Raheel <113178849+raheelimran-10P@users.noreply.github.com>
2023-02-16 12:46:40 +0500

    Resolved warning

--- Window/Tests/conftest.py
+++ Window/Tests/conftest.py
@@ -7,7 +7,7 @@ def setup(request):
     desired_caps = {
         "app": "Microsoft.WindowsCalculator_8wekyb3d8bbwe!App"
     }
-    app_driver = webdriver.Remote(command_executor='http://127.0.0.1:4723', desired_capabilities=desired_caps)
+    app_driver = webdriver.Remote(command_executor='http://127.0.0.1:4723', desired_capabilities=desired_caps, direct_connection=False)
     request.cls.driver = app_driver
     yield
     app_driver.quit()
