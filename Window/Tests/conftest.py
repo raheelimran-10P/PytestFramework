4bd8027f0f92d2def5fa181b634f04cb5f72b326
Muhammad Raheel <113178849+raheelimran-10P@users.noreply.github.com>
2023-02-20 10:59:55 +0500

    done

--- Window/Tests/conftest.py
+++ Window/Tests/conftest.py
@@ -8,17 +8,18 @@ import os
 
 @pytest.fixture()
 def setup(request):
-    os.startfile(TestData.WIN_APP_DRIVER_EXE_DIR)
+    # os.startfile(TestData.WIN_APP_DRIVER_EXE_DIR)
     # time.sleep(3)
-    desired_caps = {
-        "app": TestData.COLLECTOR_EXE_DIR
-    }
-    app_driver = webdriver.Remote(command_executor='http://127.0.0.1:4723', desired_capabilities=desired_caps,
-                                  direct_connection=False)
-    request.cls.driver = app_driver
-    yield
-    app_driver.quit()
-    terminate(TestData.WIN_APP_DRIVER_EXE)
+    # desired_caps = {
+    #     "app": TestData.COLLECTOR_EXE_DIR
+    # }
+    # app_driver = webdriver.Remote(command_executor='http://127.0.0.1:4723', desired_capabilities=desired_caps,
+    #                               direct_connection=False)
+    # request.cls.driver = app_driver
+    # yield
+    # app_driver.quit()
+    # terminate(TestData.WIN_APP_DRIVER_EXE)
+    pass
 
 
 def terminate(process_name):
--- Window/Tests/test_windows.py
+++ Window/Tests/test_windows.py
@@ -16,30 +16,10 @@ class TestWindow(BaseTest):
             element_to_check_for_dict = self.driver.create_web_element(element_id=first_element_value)
         return element_to_check_for_dict
 
-    def connect_telenium(self, host="127.0.0.1", port=4723, timeout=5):
-        """Connect to a remote telenium kivy module
-        """
-        return TeleniumHttpClient(f"http://{host}:{port}/jsonrpc", timeout=timeout)
-
     def test_01(self):
-        # el = self.driver.find_element(AppiumBy.CUSTOM, '//MDRaisedButton[@text="Connect"]')
-        # el = self.get_web_element_from_dict_if_it_is(el)
-
-        # el2 = self.driver.find_element(AppiumBy.NAME, 'Export Data')
-        # el2 = self.get_web_element_from_dict_if_it_is(el2)
-        # el2.click()
-
-        # el3 = self.driver.find_element(AppiumBy.NAME, 'Seven')
-        # el3 = self.get_web_element_from_dict_if_it_is(el3)
-        # el3.click()
-        #
-        # el4 = self.driver.find_element(AppiumBy.NAME, 'Clear entry')
-        # el4 = self.get_web_element_from_dict_if_it_is(el4)
-        # el4.click()
-        pass
-
-    def test_02(self):
-        cli = self.connect_telenium()
-        cli.wait('//MDRaisedButton[@text="Connect"]')
-        cli.wait_click('//MDRaisedButton[@text="Connect"]')
+        # go to kivy app | cd app
+        # python -m telenium.execute main.py
+        cli = connect()
+        cli.wait('//MDNavigationRailItem[@text="Connect"]', 5)
+        cli.wait_click('//MDRaisedButton[@text="Connect"]', 5)
         cli.sleep(10)
